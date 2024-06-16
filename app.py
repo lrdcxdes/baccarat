865121b348c5d1c9e9405cfd661f659a960427c7
lrdcxdes <lordgrief176@gmail.com>
2024-06-16 22:57:02 +0300

    good

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import asyncio
 import time
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 from fastapi import FastAPI, WebSocket
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from starlette.requests import Request
+from starlette.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
 import random
 from enum import Enum
@@ -18,9 +19,74 @@ templates = Jinja2Templates(directory="html")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get("/", response_class=HTMLResponse)
+def get_username(request: Request):
+    return request.cookies.get("username", None)
+
+
+@app.get("/", response_class=RedirectResponse, status_code=302)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    username = get_username(request)
+    if username:
+        return RedirectResponse(url="/game")
+    return RedirectResponse(url="/start")
+
+
+@app.get("/game", response_class=HTMLResponse)
+async def game(request: Request):
+    username = get_username(request)
+    return templates.TemplateResponse(
+        "game.html", {"request": request, "username": username}
+    )
+
+
+@app.get("/start", response_class=HTMLResponse)
+async def start(request: Request):
+    return templates.TemplateResponse("start.html", {"request": request})
+
+
+@app.get("/login", response_class=RedirectResponse, status_code=302)
+async def login(request: Request):
+    response = RedirectResponse(url="/game")
+    response.set_cookie("username", request.query_params["username"])
+    return response
+
+
+@app.get("/admin/login", response_class=HTMLResponse)
+async def admin_login(request: Request):
+    return templates.TemplateResponse("admin_login.html", {"request": request})
+
+
+@app.post("/admin/login", response_class=RedirectResponse)
+async def admin_login_post(request: Request):
+    response = RedirectResponse(url="/admin", status_code=302)
+    response.set_cookie("admin", (await request.form())["password"])
+    return response
+
+
+@app.get("/admin", response_class=HTMLResponse)
+async def admin(request: Request):
+    if request.cookies.get("admin") != "password":
+        return RedirectResponse(url="/admin/login")
+    return templates.TemplateResponse("admin.html", {"request": request})
+
+
+@app.post("/admin/change_balance")
+async def change_balance(request: Request):
+    if request.cookies.get("admin") != "password":
+        return RedirectResponse(url="/admin/login")
+    data = await request.form()
+    player = game.players.get(data["username"])
+    if not player:
+        return {"error": "Player not found"}
+    old_balance = player.balance
+    player.balance = int(data["balance"])
+    actioned = player.balance - old_balance
+    if old_balance > int(data["balance"]):
+        action = "remove"
+    else:
+        action = "win"
+    await player.send({"type": "balance", "balance": player.balance, "action": action, "actioned": actioned})
+    return {"message": "Balance updated"}
 
 
 class Deck:
@@ -57,6 +123,8 @@ class Deck:
 class BetType(str, Enum):
     player = "player"
     banker = "banker"
+    player_pair = "player_pair"
+    banker_pair = "banker_pair"
     tie = "tie"
 
 
@@ -71,17 +139,52 @@ class Bet:
 
 
 class Player:
-    def __init__(self, name, socket):
+    def __init__(self, name, socket, on_disconnect):
         self.name = name
         self.socket = socket
-        self.bets: List[Bet] = []
+        self.bet: Bet = None
+        self.balance = 1000
+        self.on_disconnect = on_disconnect
 
     async def send(self, message):
-        await self.socket.send_json(message)
+        try:
+            await self.socket.send_json(message)
+        except (WebSocketDisconnect, RuntimeError):
+            await self.on_disconnect(self)
 
-    async def bet(self, bet_type: BetType, bet: int):
-        self.bets.append(Bet(self, bet_type, bet))
-        await self.send({"type": "bet", "bet": bet_type, "amount": bet})
+    async def place_bet(self, bet_type: BetType, bet: int):
+        if bet > self.balance:
+            await self.send({"type": "error", "message": "Insufficient balance"})
+            return
+        if self.bet:
+            await self.send({"type": "error", "message": "You already placed a bet"})
+            return
+        self.bet = Bet(self, bet_type, bet)
+
+    async def remove_bet(self):
+        self.bet = None
+
+    async def win(self, amount):
+        self.balance += amount
+        await self.send(
+            {
+                "type": "balance",
+                "balance": self.balance,
+                "actioned": amount,
+                "action": "win",
+            }
+        )
+
+    async def remove_balance(self, amount):
+        self.balance -= amount
+        await self.send(
+            {
+                "type": "balance",
+                "balance": self.balance,
+                "action": "remove",
+                "actioned": amount,
+            }
+        )
 
     def __str__(self):
         return self.name
@@ -113,7 +216,7 @@ def score(cards):
 class Game:
     def __init__(self):
         self.history = []
-        self.players: List[Player] = []
+        self.players: dict[str, Player] = {}
         self.deck = Deck()
         self.last_time = time.time()
         self.is_active = False
@@ -138,7 +241,14 @@ class Game:
     async def deal_valid(self):
         self.is_active = True
         await self.broadcast({"type": "start"})
-        # await asyncio.sleep(2)
+
+        for player_name in list(self.players):
+            player = self.players[player_name]
+            if player.bet:
+                await player.remove_balance(player.bet.bet)
+
+        await asyncio.sleep(1)
+
         self.banker_cards = [self.deck.draw()]
         self.banker_score = score(self.banker_cards)
         await self.broadcast(
@@ -216,12 +326,27 @@ class Game:
                 }
             )
         # determine winner
-        if self.player_score > self.banker_score:
+        if (
+            self.player_cards[0].split("_")[0] == self.player_cards[1].split("_")[0]
+            and len(self.player_cards) == 2
+        ):
+            winner = "player_pair"
+            x = 10.0
+        elif (
+            self.banker_cards[0].split("_")[0] == self.banker_cards[1].split("_")[0]
+            and len(self.banker_cards) == 2
+        ):
+            winner = "banker_pair"
+            x = 10.0
+        elif self.player_score > self.banker_score:
             winner = "player"
+            x = 2.0
         elif self.player_score < self.banker_score:
             winner = "banker"
+            x = 1.9
         else:
             winner = "tie"
+            x = 8.0
         self.history.append(
             {
                 "banker": self.banker_cards,
@@ -242,36 +367,28 @@ class Game:
                 "winner": winner,
             }
         )
+
+        # find winners
+        for player_name in list(self.players):
+            player = self.players[player_name]
+            if player.bet:
+                if player.bet.bet_type == winner:
+                    await player.win(player.bet.bet * x)
+                await player.remove_bet()
+
         self.deck = Deck()
 
         self.is_active = False
 
-    async def add_player(self, player: Player):
-        try:
-            self.players.append(player)
-            await player.send({"type": "history", "history": self.history})
-            if game.is_active:
-                await player.send(
-                    {
-                        "type": "game",
-                        "banker": {
-                            "cards": game.banker_cards,
-                            "score": game.banker_score,
-                        },
-                        "player": {
-                            "cards": game.player_cards,
-                            "score": game.player_score,
-                        },
-                    }
-                )
-        except WebSocketDisconnect:
-            await self.remove_player(player)
+    async def add_player(self, name: str, player: Player):
+        self.players[name] = player
 
     async def remove_player(self, player: Player):
-        self.players.remove(player)
+        self.players.pop(player.name)
 
     async def broadcast(self, message):
-        for player in self.players:
+        for player_name in list(self.players):
+            player = self.players[player_name]
             try:
                 await player.send(message)
             except WebSocketDisconnect:
@@ -293,18 +410,54 @@ async def startup_event():
 @app.websocket("/game")
 async def game_socket(websocket: WebSocket):
     await websocket.accept()
+
+    player = None
+
     while True:
         try:
             data = await websocket.receive_json()
         except WebSocketDisconnect:
             break
+
         if data["type"] == "name":
-            player = Player(data["name"], websocket)
-            await game.add_player(player)
+            if data["name"] in game.players:
+                player = game.players[data["name"]]
+                player.socket = websocket
+            else:
+                player = Player(data["name"], websocket, game.remove_player)
+                await game.add_player(data["name"], player)
+            await player.send({"type": "history", "history": game.history})
+            await player.send({"type": "balance", "balance": player.balance})
+            if game.is_active:
+                await player.send(
+                    {
+                        "type": "game",
+                        "banker": {
+                            "cards": game.banker_cards,
+                            "score": game.banker_score,
+                        },
+                        "player": {
+                            "cards": game.player_cards,
+                            "score": game.player_score,
+                        },
+                    }
+                )
         elif data["type"] == "bet":
+            if game.is_active:
+                await player.send(
+                    {"type": "error", "message": "Game is already active"}
+                )
+                continue
             value = data["value"]
             amount = data["amount"]
-
+            await player.place_bet(value, amount)
+        elif data["type"] == "unbet":
+            if game.is_active:
+                await player.send(
+                    {"type": "error", "message": "Game is already active"}
+                )
+                continue
+            await player.remove_bet()
 
 
 if __name__ == "__main__":
